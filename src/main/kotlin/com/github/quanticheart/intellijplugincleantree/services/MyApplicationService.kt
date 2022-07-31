4d04610d63ee8b3f528e815626b3d8de0ea6e4cb
GitHub Action <action@github.com>
2022-07-31 03:36:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/quanticheart/intellijplugincleantree/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.quanticheart.intellijplugincleantree.services
+
+import com.github.quanticheart.intellijplugincleantree.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/quanticheart/intellijplugincleantree/services/MyProjectService.kt
+++ src/main/kotlin/com/github/quanticheart/intellijplugincleantree/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.quanticheart.intellijplugincleantree.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.quanticheart.intellijplugincleantree.MyBundle
 
 class MyProjectService(project: Project) {
 
